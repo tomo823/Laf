2bd52e3864877c15a57ed19117f99b34346244e0
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-06-30 00:54:07 +0000

    truly completed log.py

--- log.py
+++ log.py
@@ -9,17 +9,19 @@ with open("script.log", "r") as f:
     s = f.read()
     #URLの抽出
     urls = re.findall(r"https://www.youtube.com/watch\?v=[a-zA-Z0-9]{11}", s) 
+    #Titleの厳選
     for i in re.findall("【.*】", s):
         if len(i) > 10:
             l.append(i)
 
-print(l)
-
+#Titleの整理,listに格納
 for i in range(int(len(l)/3)):
     list.append(l[2*i+2])
-    
-print(list)
-print(urls)
+
+for i in range(len(urls)):
+    dict[urls[i]] = list[i]
+
+print(dict)
 
 """with open("script.log", "w") as file:
     pass"""
