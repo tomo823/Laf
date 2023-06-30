e817b89d454ae2660dce955920a483b90a0c45f1
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-06-30 00:49:37 +0000

    completed log.py

--- log.py
+++ log.py
@@ -8,12 +8,16 @@ dict = {}
 with open("script.log", "r") as f:
     s = f.read()
     #URLの抽出
-    urls = re.findall(r"https://www.youtube.com/watch\?v=[a-zA-Z0-9]{11}", s)
-    l = re.findall("【.*】", s)
-    for i in l:
+    urls = re.findall(r"https://www.youtube.com/watch\?v=[a-zA-Z0-9]{11}", s) 
+    for i in re.findall("【.*】", s):
         if len(i) > 10:
-            list.append(i)
+            l.append(i)
 
+print(l)
+
+for i in range(int(len(l)/3)):
+    list.append(l[2*i+2])
+    
 print(list)
 print(urls)
 
