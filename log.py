d0af4ff768b6fad692f9a41a3daf582c69757cab
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-06-30 05:29:22 +0000

    modified:   log.py

--- log.py
+++ log.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 
 list = []
@@ -13,15 +14,41 @@ with open("script.log", "r") as f:
     for i in re.findall("【.*】", s):
         if len(i) > 10:
             l.append(i)
+    f.close()
+
 
 #Titleの整理,listに格納
 for i in range(int(len(l)/3)):
     list.append(l[2*i+2])
 
+#Dict作成
 for i in range(len(urls)):
     dict[urls[i]] = list[i]
 
+#As confirmation
 print(dict)
+print()
 
+
+"""
+#Make a text file for URL
+with open("URL.txt", "w") as file:
+    json.dump(dict, file)"""
+
+
+#Opening text file and modify it
+f = open("URL.txt", "r")
+new_dict = json.load(f)
+print(new_dict)
+print()
+    
+dict.update(new_dict)
+
+print(dict)
+with open("URL.txt", "w") as file:
+    json.dump(dict, file)
+
+
+#Delete to write other logs
 """with open("script.log", "w") as file:
     pass"""
